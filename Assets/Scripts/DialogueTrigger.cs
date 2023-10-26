8631043fab0cb8836d84699819bcf78f3f5255eb
ricop7 <enricofpellini@gmail.com>
2023-10-26 15:33:46 -0400

    Finish Intro Scene

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,28 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI; // You'll need this for working with UI elements
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-   // private Animator fade;
+    //public Button dialogueButton;
+    private bool dialogueTriggered = false;
+
+      public TextMeshProUGUI textToDisable;
+
     void Start()
     {
-        //fade = GetComponent<Animator>();
+
     }
 
     public void TriggerDialogue()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-        //fade.Play("fading");
-        Fade();
-        //gameObject.SetActive(true);
+        DeleteText();
+        if (!dialogueTriggered)
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            Fade();
+            dialogueTriggered = true;
+            // Disable the button or the component
+            //dialogueButton.interactable = false;
+
+        }
+    }
+
+    public void DeleteText()
+    {
+        if (textToDisable != null)
+        {
+            textToDisable.gameObject.SetActive(false);
+        }
     }
 
     IEnumerator Fade()
     {
         yield return new WaitForSeconds(0.2f);
-        //Destroy(gameObject);
-        gameObject.SetActive(true);
+        //gameObject.SetActive(false);
     }
 }
